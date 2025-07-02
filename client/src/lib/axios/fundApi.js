ed33b038a459fbbd2f90c9c5070ec36b0a58783b
realsoumyadey07 <soumyadipdey802@gmail.com>
2025-07-02 10:25:18 +0530

    ok

--- client/src/lib/axios/fundApi.js
+++ client/src/lib/axios/fundApi.js
@@ -3,7 +3,6 @@ import { getEnv } from "../common";
 
 export const fundApi = axios.create({
      baseURL: getEnv("VITE_BASE_URL"),
-     // withCredentials: true,
      headers: {
           "Content-Type": "application/json"
      }
